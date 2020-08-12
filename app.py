102d51d857ed62cf7ae67e8e5ae03cc85dd9b58b
Nathan Drew <nath.drew@gmail.com>
2020-08-12 15:55:40 +0100

    Routing updates and redirect from root

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import socket
 import flask
-from flask import jsonify
+from flask import jsonify, redirect, url_for
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -8,6 +8,11 @@ app.config["DEBUG"] = True
 payload = {'backend_host': socket.gethostname()}
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
+def index():
+    return redirect(url_for('home'))
+
+
+@app.route('/v1/host', methods=['GET'])
 def home():
     return jsonify(payload)
